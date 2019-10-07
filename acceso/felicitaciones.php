4dd181e25915bc102b809d489927289b25e0e1e8
bfsabio <bfsabio@hotmail.com>
2019-10-07 13:59:31 -0300

    tp detodo3d

---
+++ acceso/felicitaciones.php
@@ -0,0 +1,19 @@
+<?php
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+    <?php
+    include '../includes/navbar.php';
+
+    ?>
+    <div class="aling" >
+    <a href="">usuario ingresado</a>
+    </div>
+   </body>
+ </html>
