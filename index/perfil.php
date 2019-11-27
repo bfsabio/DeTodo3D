6e134da08bcd2ea1b03c3818689b34c35ef98a58
eduardoadrian1994 <53192145+eduardoadrian1994@users.noreply.github.com>
2019-11-27 13:33:43 -0300

    corecciones

--- index/perfil.php
+++ index/perfil.php
@@ -41,12 +41,12 @@ if ($usuarioLogueado == null) {
       </div>
 
       <div class="text-center col-md-2">
-        <p> NOMBRE DEL PERFIL</p> <!-- PHP EMBEBIDO DONDE SE MUESTRE EL NOMBRE DE LA EMPRESA -->
+        <p class="texto"> NOMBRE DEL PERFIL</p> <!-- PHP EMBEBIDO DONDE SE MUESTRE EL NOMBRE DE LA EMPRESA -->
       </div>
 
       <div class="text-left col-md-8">
-        <p>sobre mi:</p><br> <!-- PHP EMBEBIDO DONDE SE MUESTRE EL NOMBRE DE LA EMPRESA -->
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="texto">sobre mi:</p><br> <!-- PHP EMBEBIDO DONDE SE MUESTRE EL NOMBRE DE LA EMPRESA -->
+        <p class="texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
 
     </div>
@@ -63,11 +63,11 @@ if ($usuarioLogueado == null) {
     </div>
 
     <div class="text-left col-md-12">
-      <p>Mis datos:</p><br> <!-- PHP EMBEBIDO DONDE SE MUESTRE EL NOMBRE DE LA EMPRESA -->
-      <p>email:...</p><br>
-      <p>contraseña:...</p><br>
-      <p>celular:...</p><br>
-      <p>direcion:...</p>
+      <p class="texto">Mis datos:</p><br> <!-- PHP EMBEBIDO DONDE SE MUESTRE EL NOMBRE DE LA EMPRESA -->
+      <p class="texto">email:...</p><br>
+      <p class="texto">contraseña:...</p><br>
+      <p class="texto">celular:...</p><br>
+      <p class="texto">direcion:...</p>
     </div>
 
   </div>
