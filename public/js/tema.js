aa9f0060282fb97d1fe79b3184fdd9f32326ea63
bfsabio <bfsabio@hotmail.com>
2020-01-09 01:18:42 -0300

    script 5 cambio de tema
    cambio de estilo.
    funciones en js en nabvar cambio de fondo, color navbar y texto.
    falta establecerlo fijo
    y colocarlo en el register en base de datos a modo true o false

---
+++ public/js/tema.js
@@ -0,0 +1,16 @@
+
+      var boton = document.getElementById('tema');
+
+      boton.onclick=function() {
+        document.body.style.backgroundImage="url('img/fondo2.jpg')";
+        document.getElementById("color").style.background="#E3E1E1";
+        document.getElementById("text").style.color="black";
+        document.getElementById("text1").style.color="black";
+        document.getElementById("text2").style.color="black";
+      };
+
+      // function myFunction() {
+      //
+      // document.getElementById("fondo").style.backgroundColor = 'red';
+      // document.getElementById("demo").innerHTML = "Hello World";
+      // }
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <title>DeTodo3D</title>
+    <script src="js/cargarAjax.js"></script>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="<?php echo asset('css/navbar.css')?>" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -13,23 +13,27 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark" id="color" >  <!-- bg-dark -->
         <a class="navbar-brand" href="/"> <img src="https://1.bp.blogspot.com/-PJY1pxWjQrc/XdV3848u4bI/AAAAAAAAd8k/ziA-rafAqjgbTKVPxQtF7coDM0PiIpf2ACNcBGAsYHQ/s1600/logo.png" alt="logo" width="150px"> </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse"
           data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item"><a class="nav-link @if(Request::is('login'))disabled @endif" href="login">Ingresar <span class="sr-only">(current)</span></a></li>
-            <li class="nav-item"><a class="nav-link @if(Request::is('register'))disabled @endif" href="registrar">Registrarse</a></li>
-            <li class="nav-item"><a class="nav-link @if(Request::is('faq'))disabled @endif" href="faq">Faq</a></li>
+          <ul class="navbar-nav mr-auto" >
+            <li  class="nav-item"><a id="text"  class="nav-link @if(Request::is('login'))disabled @endif" href="login">Ingresar <span class="sr-only">(current)</span></a></li>
+            <li  class="nav-item"><a id="text1" class="nav-link @if(Request::is('register'))disabled @endif" href="registrar">Registrarse</a></li>
+            <li  class="nav-item"><a id="text2" class="nav-link @if(Request::is('faq'))disabled @endif" href="faq">Faq</a></li>
           </ul>
+          <section>
+            <button class="tema" id="tema">estilo</button>
+          </section>
           <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>
       </nav>
+          <script src="js/tema.js"></script>
     </body>
   </html>
